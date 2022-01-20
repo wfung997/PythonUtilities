f62a59fcbd5df9b3fd76761b4e3f08062f241d6f
William Fung <wfung@liquidnet.com>
2022-01-20 15:51:28 +0800

    add code

--- parse_fix.py
+++ parse_fix.py
@@ -1,7 +1,8 @@
 import io
 import re
+import argparse
 
-def parse_order_messages(f, tags_to_print,out_file=None):
+def parse_order_messages(f, tags_to_print,out_file=None, sep="|"):
     tags_to_print.sort()
     line_count=0
     records=[]
@@ -11,7 +12,7 @@ def parse_order_messages(f, tags_to_print,out_file=None):
         line_count+=1
         fix_msg = re.search(r"\[(.*?)]", line).group(1)
         # print(fix_msg)
-        values=fix_msg.split('|')
+        values=fix_msg.split(sep)
         values.remove('')
         # print(values)
         order={}
@@ -26,9 +27,6 @@ def parse_order_messages(f, tags_to_print,out_file=None):
             record+=order[tag] if tag in order else ''
             record+=','
         records.append(record)
-        
-
-        # for tag in tags_to_print: 
 
     print(f'{line_count=}')
     if out_file==None:
@@ -41,16 +39,24 @@ def parse_order_messages(f, tags_to_print,out_file=None):
             for record in records:
                 output.write(f'{record}\n')
 
-def main():
-    in_file='C:\\in.log'
-    out_file='C:\\out.log'
+def menu():
+    parser = argparse.ArgumentParser(description='Command line application example')
+    parser.add_argument('-i','--in_fix', help='Input FIX log', type=argparse.FileType('r'), required=True)
+    parser.add_argument('-o','--out_fix', help='Output FIX log. If default=None, print to screen ', default=None, required=False)
+    parser.add_argument('-s','--fix_sep', help='FIX tag/value separator', default="|", required=False)
+    parser.add_argument('-tags','--tags', help='List of tags to extract, separeted by comma', default="11,54,48,22,38,52,60,40,44", required=False)
+    args = vars(parser.parse_args())
+    return args 
 
-    # with  open (in_File) as f:
-    #     tags_to_print =[11,54,48,22,38,52,60,40,44]
-    #     parse_order_messages(f,tags_to_print)
-    with  open (in_file) as f:
-        tags_to_print =[11,39,41,52,60,14]
-        parse_order_messages(f, tags_to_print,out_file)
+def main():
+    args = menu()
+    # print(args)
+    in_file=args['in_fix']
+    out_file=args['out_fix']
+    fix_sep=args['fix_sep']
+
+    tags_to_print = list(map(int, args['tags'].split (','))) 
+    parse_order_messages(in_file, tags_to_print, out_file, sep=fix_sep)
 
 if __name__ == '__main__':
     main()
