6a5db75ec15c75ea560b60a25d34ed9b4cc29d6d
willifwl <willifwl@gmail.com>
2022-04-21 22:36:40 +0800

    add simple logger

---
+++ logger.py
@@ -0,0 +1,14 @@
+import datetime
+import logging
+
+def initializeLogging(config):
+    now = datetime.now().strftime("%Y%m%d_%H%M%S")
+    app_name = "myapp"
+    log_filename = f"{app_name}_{now}.log"
+    print(f'Writing logs to {log_filename}')
+    level = logging.DEBUG
+    log_format = "%(levelname)s % (asctime)s - %(message)s"
+    datefmt = '%Y-%m-%d %H:%M:%S'
+    logging.basicConfig(filename=log_filename, format=format,
+                        datefmt=datefmt, level=level)
+    logging.info(f"Start logging {app_name}")
